4309deeedac930bbb89e93ce88dc9c38d615ae6e
cybergaz <kkanttechy@gmail.com>
2024-08-28 14:04:15 +0530

    initial commit

---
+++ lib/constants.ts
@@ -0,0 +1,13 @@
+export const isProduction = process.env.NODE_ENV === 'production'
+
+export const SITE_URL = isProduction ? 'https://github.com/cybergaz/portfolio' : 'http://localhost:3001'
+export const SITE_NAME = 'Gaz'
+export const SITE_TITLE = 'Krishan Kant | Software Developer'
+export const SITE_DESCRIPTION = 'Krishan Kant • Software Developer • Open-Source Enthusiast'
+export const SITE_KEYWORDS = ['cybergaz', 'gaz', 'portfolio', 'about cybergaz', 'Next.js', 'React', 'TypeScript', 'Node.js']
+
+export const GITHUB_URL = 'https://github.com/cybergaz'
+export const INSTAGRAM_URL = 'https://www.instagram.com/cyber_gaz'
+export const X_URL = 'https://x.com/gaz_guine'
+export const LINKEDIN_URL = 'https://www.linkedin.com/in/krishna-kant-bb372b212'
+
