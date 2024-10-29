07c2bff7493eeb25c93e5468a2e6d412d157a33d
cybergaz <kkanttechy@gmail.com>
2024-10-29 20:46:57 +0530

    update: links

--- lib/constants.ts
+++ lib/constants.ts
@@ -6,14 +6,15 @@ export const SITE_TITLE = 'Krishan Kant | Software Developer'
 export const SITE_DESCRIPTION = 'Krishan Kant • Software Developer • Open-Source Enthusiast'
 export const SITE_KEYWORDS = ['cybergaz', 'gaz', 'krishan kant', 'portfolio', 'about cybergaz', 'Next.js', 'React', 'TypeScript']
 
-export const SOCIALS = {
+export const LINKS = {
     github: 'https://github.com/cybergaz',
     instagram: 'https://www.instagram.com/cyber_gaz',
     linkedin: 'https://www.linkedin.com/in/cybergaz',
     whatsapp: 'https://wa.me/918534946840',
     telegram: 'https://t.me/cyber_gaz',
     x: 'https://x.com/gaz_guine',
-    email: 'mailto:gaz.sync@gmail.com'
+    email: 'mailto:gaz.sync@gmail.com',
+    resume: 'https://docs.google.com/document/d/15S-Iosgq6zDaIkpsCFT3Ln--NTU4O3qo/edit?usp=sharing&ouid=114908455304794398126&rtpof=true&sd=true'
 }
 
 export const NAVLINKS = [
