25b943c98ced3894c10042455aa8dc3470b72fa9
cybergaz <kkanttechy@gmail.com>
2024-12-18 13:48:10 +0530

    style: update button layout

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -9,13 +9,14 @@ const Button: React.FC<ButtonProps> = ({ className, children, ...props }) => {
     return (
         <button
             className={cn(
-                'px-6 py-1.5 border dark:bg-blue-300/10 bg-blue-400/10 hover:bg-blue-400/20 dark:hover:bg-blue-400/20 border-blue-500/20 text-foreground mx-auto text-center rounded-full relative',
+                'group px-6 py-1.5 border bg-blue-500/5 hover:bg-blue-500/0 border-blue-500/20 text-foreground mx-auto text-center rounded-full relative',
                 className
             )}
             {...props}
         >
+            <div className="absolute h-px opacity-0 group-hover:opacity-100 transition-all duration-500 ease-in-out inset-y-0 bg-gradient-to-r w-3/4 mx-auto from-transparent dark:via-blue-500 via-blue-600 to-transparent" />
             {children}
-            <div className="absolute inset-x-0 h-px -bottom-px bg-gradient-to-r w-3/4 mx-auto from-transparent dark:via-blue-500 via-blue-600 to-transparent" />
+            <span className="absolute group-hover:opacity-30 transition-all duration-500 ease-in-out inset-x-0 h-px -bottom-px bg-gradient-to-r w-3/4 mx-auto from-transparent dark:via-blue-500 via-blue-600 to-transparent" />
         </button>
     );
 };
