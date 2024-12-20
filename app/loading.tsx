1167328d12e898c2b8878c1fbfede4ca6454c8e9
cybergaz <kkanttechy@gmail.com>
2024-12-20 22:39:42 +0530

    chore: update loading animations

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,5 +1,11 @@
+import Image from "next/image"
 
 export default function Loading() {
     // Or a custom loading skeleton component
-    return <p className="h-screen w-screen flex justify-center items-center">Loading...</p>
+    return (
+        <div className="h-screen w-screen flex flex-col gap-5 text-2xl justify-center items-center">
+            <Image src={"/images/spinner.svg"} alt="laptop svg" width={60} height={60} className="dark:invert animate-spin" />
+            wait a sec . . .
+        </div>
+    )
 }
