a0981b9e4442082498ea8188ade361b5d0147cb0
cybergaz <kkanttechy@gmail.com>
2025-02-07 21:31:29 +0530

    build fix

--- components/ui/animated-tabs.tsx
+++ components/ui/animated-tabs.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useState } from "react";
-import { motion } from "framer-motion";
+import { motion } from "motion/react";
 import { cn } from "@/lib/utils";
 
 export interface Tab {
